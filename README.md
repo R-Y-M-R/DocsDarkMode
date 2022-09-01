5b1cf94b3c641b12ee5ede19b4e3c0ad8c1ca810
R-Y-M-R <32427668+R-Y-M-R@users.noreply.github.com>
2022-09-01 19:44:00 -0400

    V1

---
+++ README.md
@@ -0,0 +1,19 @@
+# Why?
+
+Google docs is blinding white.
+
+# Contributions
+
+Welcome, open a PR.
+
+# Credit to the original author
+
+Credit to whoever wrote "[Google Docs Dark Mode](https://chrome.google.com/webstore/detail/google-docs-dark-mode/lgjhepbpjcmfmjlpkkdjlbgomamkgonb)" as I am using their code as a base.
+
+I hated the gray and just wanted to darken it up, so here we are. Thanks for your extension pushing me to create a copy of it.
+
+# Donate
+
+If you would like to generously donate, please send it to my bitcoin address.
+
+[bc1qpzhx9lve4luqmu0gdnzdy94t2mxwx86j76dfa3](bitcoin:bc1qpzhx9lve4luqmu0gdnzdy94t2mxwx86j76dfa3)
--- content.js
+++ content.js
@@ -1,44 +1,44 @@
-var cssId = 'darkModeStylesheet';
+var cssId = "darkestBlackModeStylesheet";
 try {
-    var head = document.getElementsByTagName('head')[0];
-    var link = document.createElement('link');
-    link.id = cssId;
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = chrome.runtime.getURL('css/dark_mode_docs.css');
-    link.media = 'all';
-    head.appendChild(link);
-    console.log("Dark mode enabled successfully!");
+  var head = document.getElementsByTagName("head")[0];
+  var link = document.createElement("link");
+  link.id = cssId;
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = chrome.runtime.getURL("css/dark_mode_docs.css");
+  link.media = "all";
+  head.appendChild(link);
+  console.log("Dark mode enabled successfully!");
 } catch (err) {
-    console.log("Error while loading dark mode: ", err);
+  console.log("Error while loading dark mode: ", err);
 }
 
-chrome.storage.sync.get(['GDDM-active'], function (result) {
-    if (result['GDDM-active'] === "false") {
-        if (document.getElementById(cssId)) {
-            let linkToCSS = document.getElementById(cssId);
-            linkToCSS.parentElement.removeChild(linkToCSS);
-        }
+chrome.storage.sync.get(["GDDM-active"], function (result) {
+  if (result["GDDM-active"] === "false") {
+    if (document.getElementById(cssId)) {
+      let linkToCSS = document.getElementById(cssId);
+      linkToCSS.parentElement.removeChild(linkToCSS);
     }
+  }
 });
 chrome.runtime.onMessage.addListener(function (message) {
-    if (message === "true") {
-        // If link is not there, add it
-        if (!document.getElementById(cssId)) {
-            var head = document.getElementsByTagName('head')[0];
-            var link = document.createElement('link');
-            link.id = cssId;
-            link.rel = 'stylesheet';
-            link.type = 'text/css';
-            link.href = chrome.runtime.getURL('css/dark_mode_docs.css');
-            link.media = 'all';
-            head.appendChild(link);
-        }
-    } else {
-        // If the link is there, remove it
-        if (document.getElementById(cssId)) {
-            let linkToCSS = document.getElementById(cssId);
-            linkToCSS.parentElement.removeChild(linkToCSS);
-        }
+  if (message === "true") {
+    // If link is not there, add it
+    if (!document.getElementById(cssId)) {
+      var head = document.getElementsByTagName("head")[0];
+      var link = document.createElement("link");
+      link.id = cssId;
+      link.rel = "stylesheet";
+      link.type = "text/css";
+      link.href = chrome.runtime.getURL("css/dark_mode_docs.css");
+      link.media = "all";
+      head.appendChild(link);
     }
+  } else {
+    // If the link is there, remove it
+    if (document.getElementById(cssId)) {
+      let linkToCSS = document.getElementById(cssId);
+      linkToCSS.parentElement.removeChild(linkToCSS);
+    }
+  }
 });
